4db81f1642423a5802e457c4dc895c66c0e65b82
Kurt Newell <Kurt Newell@K583794.lan>
2020-04-21 21:08:52 +1000

    feature(set up):  finishes feature

--- learn-lwjgl/src/main/java/render/Main.java
+++ learn-lwjgl/src/main/java/render/Main.java
@@ -3,11 +3,40 @@ package render;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main {
+public class Main implements Runnable{
   private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
   
+  public Thread game;
+  
+  public void start() {
+    game = new Thread(this, "game");
+    game.start();
+  }
+  
+  public static void init() {
+    LOGGER.debug("Initialising Game");    
+  }
+  
+  public void run() {
+    init();
+    
+    while (true) {
+      update();
+      render();
+    }
+  }
+  
+  private void render() {
+    LOGGER.debug("Rendering Game");        
+  }
+
+  private void update() {
+    LOGGER.debug("Updating Game");        
+  }
+
   public static void main(String[] args) {
-    LOGGER.debug("Hello World");
+    
+    new Main().start();
 
   }
 
